14684c0924d8ede24e0bb1bed5e5566ab608178d
Jack Baron <jackmbaron@gmail.com>
2023-07-31 16:52:41 +0100

    use external @dreamlab.gg/core

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,49 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          matter: ['matter-js'],
-          pixi: ['pixi.js'],
-          dreamlab: ['@dreamlab.gg/core'],
-        },
+/** @returns {import('vite').Plugin} */
+const importMapPlugin = () => ({
+  name: 'import-map',
+  transformIndexHtml: () => {
+    const pkg = '@dreamlab.gg/core'
+    const version = '0.0.13' // TODO: Resolve version from package.json
+    const resolved = `https://esm.sh/${pkg}@${version}/bundled`
+
+    const map = {
+      imports: {
+        [pkg]: resolved,
       },
-    },
+    }
+
+    // TODO: Resolve modules dynamically
+    const modules = [
+      'entities',
+      'input',
+      'math',
+      'network',
+      'textures',
+      'utils',
+    ]
+
+    for (const module of modules) {
+      map.imports[`${pkg}/${module}`] = resolved
+    }
+
+    return [
+      {
+        tag: 'script',
+        attrs: { type: 'importmap' },
+        children: JSON.stringify(map),
+      },
+    ]
   },
 })
+
+export default defineConfig(async () => ({
+  plugins: [importMapPlugin()],
+
+  build: {
+    rollupOptions: {
+      external: ['@dreamlab.gg/core', /@dreamlab.gg\/core\/*/g],
+    },
+  },
+}))
