ae83f2ec1120756855312c7c0d2e584d22d04b76
Jack Baron <jackmbaron@gmail.com>
2023-08-07 21:51:14 +0100

    add matter and pixi to import map

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,33 +1,89 @@
+// @ts-check
 import { defineConfig } from 'vite'
 import { resolve } from 'import-meta-resolve'
 import { readFile } from 'node:fs/promises'
 import { fileURLToPath } from 'node:url'
+import { join, parse, sep } from 'node:path'
+
+// #region Package Helpers
+/**
+ * @param {string} pkg - Package name
+ * @returns {Promise<import('type-fest').PackageJson>}
+ */
+const packageJson = async pkg => {
+  const direct = async () => {
+    const path = await resolve(`${pkg}/package.json`, import.meta.url)
+    const json = await readFile(fileURLToPath(path), 'utf8')
+
+    return json
+  }
+
+  const indirect = async () => {
+    const root = await resolve(pkg, import.meta.url)
+    const { dir } = parse(fileURLToPath(root))
+
+    const dirs = dir.split(sep)
+    const offset = pkg.startsWith('@') ? 3 : 2
+    const idx = dirs.length - dirs.indexOf('node_modules') - offset
+
+    let path = dir
+    for (let i = 0; i < idx; i++) {
+      path = join(path, '..')
+    }
+
+    const jsonPath = join(path, 'package.json')
+    const json = await readFile(jsonPath, 'utf8')
+
+    return json
+  }
+
+  // @ts-ignore
+  const json = await Promise.any([direct(), indirect()])
+  return JSON.parse(json)
+}
+
+/**
+ *
+ * @param {string} pkg - Package Name
+ */
+const esmLink = async pkg => {
+  const { version } = await packageJson(pkg)
+  if (!version) throw new Error(`unknown version for package: ${pkg}`)
+
+  return `https://esm.sh/${pkg}@${version}`
+}
+// #endregion
 
 /** @returns {import('vite').Plugin} */
 const importMapPlugin = () => ({
   name: 'import-map',
   transformIndexHtml: async () => {
-    const pkg = '@dreamlab.gg/core'
+    const core = '@dreamlab.gg/core'
+    const matter = 'matter-js'
+    const pixi = 'pixi.js'
 
-    const pkgPath = await resolve(`${pkg}/package.json`, import.meta.url)
-    const pkgJson = await readFile(fileURLToPath(pkgPath), 'utf8')
+    const [corePkg, matterPkg, pixiPkg] = await Promise.all([
+      esmLink(core),
+      esmLink(matter),
+      esmLink(pixi),
+    ])
 
-    const { version } = JSON.parse(pkgJson)
-    const resolved = `https://esm.sh/${pkg}@${version}/bundled`
-
+    const coreBundled = `${corePkg}/bundled`
     const map = {
       imports: {
-        [pkg]: resolved,
+        [matter]: matterPkg,
+        [pixi]: pixiPkg,
+        [core]: coreBundled,
       },
     }
 
-    const modulesPath = await resolve(`${pkg}/modules.json`, import.meta.url)
+    const modulesPath = await resolve(`${core}/modules.json`, import.meta.url)
     const modulesJson = await readFile(fileURLToPath(modulesPath), 'utf8')
 
     const modules = JSON.parse(modulesJson)
     for (const module of modules) {
-      map.imports[`${pkg}/${module}`] = resolved
-      map.imports[`${pkg}/dist/${module}`] = resolved
+      map.imports[`${core}/${module}`] = coreBundled
+      map.imports[`${core}/dist/${module}`] = coreBundled
     }
 
     return [
@@ -45,13 +101,10 @@ export default defineConfig(async () => ({
 
   build: {
     rollupOptions: {
-      output: {
-        manualChunks: {
-          matter: ['matter-js'],
-        },
-      },
-
       external: (source, importer, isResolved) => {
+        if (source.includes('matter-js')) return true
+        if (source.includes('pixi.js')) return true
+        if (source.includes('@pixi/')) return true
         if (source.includes('@dreamlab.gg/core')) return true
       },
     },
