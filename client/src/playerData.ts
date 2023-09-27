6f49b0e94e2b364b89c5ef4a2ee418553f725be2
CodedRed <devcodedred@gmail.com>
2023-09-27 17:36:44 -0500

    updated PlayerData

--- client/src/playerData.ts
+++ client/src/playerData.ts
@@ -1,37 +1,27 @@
 import type { ObjectItem, PlayerData } from '@dreamlab.gg/core/managers'
 import { PlayerDataManager } from '@dreamlab.gg/core/managers'
-import { TextureManager } from '@dreamlab.gg/core/textures'
 
-let objects: ObjectItem[]
 let resolveObjects: (value: ObjectItem[] | PromiseLike<ObjectItem[]>) => void
-
 const objectsPromise = new Promise<ObjectItem[]>(resolve => {
   resolveObjects = resolve
 })
 
-window.addEventListener('message', async ev => {
+const storePlayerData = (data: PlayerData) => {
+  window.localStorage.setItem('globalPassedPlayerData', JSON.stringify(data))
+}
+
+const updatePlayerManager = (data: PlayerData) => {
+  return PlayerDataManager.setAll(data)
+}
+
+export const getObjects = async () => objectsPromise
+
+window.addEventListener('message', ev => {
   const data: PlayerData = ev.data
 
   if (data?.user && data?.inputs) {
-    window.localStorage.setItem('globalPassedPlayerData', JSON.stringify(data))
-
-    const updatedData = PlayerDataManager.setAll(data)
-    if (updatedData) {
-      objects = updatedData.objects
-    }
-
-    if (objects) {
-      const promises = objects
-        .filter(obj => obj.imageTasks?.[0]?.imageURL)
-        .map(async obj =>
-          TextureManager.loadTexture(obj.imageTasks[0].imageURL),
-        )
-
-      await Promise.all(promises)
-    }
-
-    resolveObjects(objects)
+    storePlayerData(data)
+    const updatedData = updatePlayerManager(data)
+    resolveObjects(updatedData?.objects ?? [])
   }
 })
-
-export const getObjects = async () => objectsPromise
