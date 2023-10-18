d698a7e7713b856396ccfa097a5b34cccf93a748
Jackson Roberts <jacksonroberts25@gmail.com>
2023-10-18 14:10:56 -0600

    delete server folder.

--- server/src/env.ts
+++
@@ -1,5 +0,0 @@
-import { defineEnvironment, t } from '@luludev/env'
-
-export const env = defineEnvironment({
-  PORT: t.int().default(3_000),
-})
--- server/src/index.ts
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-import { env } from './env.js'
-
-const app = express()
-
-app.listen(env.PORT)
