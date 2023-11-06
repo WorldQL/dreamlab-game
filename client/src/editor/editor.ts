293be9a932b8d7ce8786dd2a2a1449c1df13c664
Jack Baron <jackmbaron@gmail.com>
2023-11-06 11:23:57 +0000

    press K to toggle physics

--- client/src/editor/editor.ts
+++ client/src/editor/editor.ts
@@ -1,6 +1,10 @@
 import { createEntity } from '@dreamlab.gg/core'
 import { createEntitySelect } from './select'
 
+enum EditorInputs {
+  Toggle = '@editor/Toggle',
+}
+
 export const createEditor = () => {
   const selector = createEntitySelect()
   // TODO: Implement the rest of the editor
@@ -8,7 +12,19 @@ export const createEditor = () => {
   return createEntity({
     async init({ game }) {
       await game.instantiate(selector)
-      return { game }
+
+      const toggleEditor = (pressed: boolean) => {
+        if (!pressed) return
+
+        if (game.physics.running) game.physics.suspend()
+        else game.physics.resume()
+      }
+
+      const inputs = game.client?.inputs
+      inputs?.registerInput(EditorInputs.Toggle, 'KeyK')
+      inputs?.addListener(EditorInputs.Toggle, toggleEditor)
+
+      return { game, toggleEditor }
     },
 
     initRenderContext(_init, _render) {
@@ -16,7 +32,10 @@ export const createEditor = () => {
       return {}
     },
 
-    async teardown({ game }) {
+    async teardown({ game, toggleEditor }) {
+      const inputs = game.client?.inputs
+      inputs?.removeListener(EditorInputs.Toggle, toggleEditor)
+
       await game.destroy(selector)
     },
 
