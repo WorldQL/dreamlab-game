ba8a7fdeebd83b54240b2dae6f674cab2bd559fe
Christopher G <devcodedred@gmail.com>
2023-12-06 15:54:22 -0600

    wip asset manager

--- src/editor/palette.tsx
+++ src/editor/palette.tsx
@@ -8,6 +8,7 @@ import cuid2 from '@paralleldrive/cuid2'
 import {
   useCallback,
   useMemo,
+  useRef,
   useState,
 } from 'https://esm.sh/v136/react@18.2.0'
 import type { FC } from 'https://esm.sh/v136/react@18.2.0'
@@ -44,6 +45,50 @@ const SpawnableList = styled.div`
   padding: 0.5rem 0;
 `
 
+const AssetUploader = styled.div`
+  border: 2px dashed rgb(99 102 241);
+  padding: 18px;
+  text-align: center;
+  cursor: pointer;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  background-color: #f8f9fa;
+  &:hover,
+  &.drag-over {
+    background-color: #e2e6ea;
+    border-color: #007bff;
+  }
+`
+
+const AssetList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  overflow-y: auto;
+`
+
+const AssetItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 5px;
+  border-radius: 5px;
+  background-color: #f8f9fa;
+  border: 1px solid #ddd;
+`
+
+const ImagePreview = styled.img`
+  width: 50px;
+  height: 50px;
+  object-fit: cover;
+  border-radius: 5px;
+`
+
+const CATEGORIES = {
+  SPAWNABLES: 'Spawnables',
+  ASSETS: 'Assets',
+}
+
 export const Palette: FC<{ readonly selector: Selector }> = ({ selector }) => {
   const game = useGame()
   const network = useNetwork()
@@ -57,6 +102,30 @@ export const Palette: FC<{ readonly selector: Selector }> = ({ selector }) => {
   const [isCollapsed, setIsCollapsed] = useState(false)
 
   const spawnedEntitiesAwaitingSelection: string[] = []
+  const [currentCategory, setCurrentCategory] = useState(CATEGORIES.SPAWNABLES)
+
+  const [assets, setAssets] = useState<{ name: string; url: string }[]>([])
+  const fileInputRef = useRef<HTMLInputElement>(null)
+
+  const handleUploadClick = () => {
+    if (!fileInputRef.current) return
+    fileInputRef.current.click()
+  }
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]
+    if (file) {
+      const reader = new FileReader()
+      reader.addEventListener('load', ev => {
+        setAssets(prev => [
+          ...prev,
+          { name: file.name, url: ev.target?.result as string },
+        ])
+      })
+
+      reader.readAsDataURL(file)
+    }
+  }
 
   // This event is being fired three times??
   game.events.common.addListener('onSpawn', entity => {
@@ -106,18 +175,62 @@ export const Palette: FC<{ readonly selector: Selector }> = ({ selector }) => {
       </CollapseButton>
       {!isCollapsed && (
         <>
-          <h1>Spawn Object</h1>
-          <SpawnableList>
-            {spawnable.map(([name]) => (
+          <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+            {Object.values(CATEGORIES).map(category => (
               <Button
-                key={name}
-                type='button'
-                onClick={async () => spawn(name)}
+                key={category}
+                onClick={() => setCurrentCategory(category)}
+                style={{
+                  backgroundColor:
+                    currentCategory === category ? 'lightgrey' : 'transparent',
+                  border: 'none',
+                  color: 'black',
+                }}
               >
-                {name}
+                {category}
               </Button>
             ))}
-          </SpawnableList>
+          </div>
+
+          {currentCategory === CATEGORIES.SPAWNABLES && (
+            <>
+              <h1>Spawn Object</h1>
+              <SpawnableList>
+                {spawnable.map(([name]) => (
+                  <Button
+                    key={name}
+                    type='button'
+                    onClick={async () => spawn(name)}
+                  >
+                    {name}
+                  </Button>
+                ))}
+              </SpawnableList>
+            </>
+          )}
+
+          {currentCategory === CATEGORIES.ASSETS && (
+            <div>
+              <h1>Assets</h1>
+              <AssetUploader onClick={handleUploadClick}>
+                Drag and drop files or click here
+                <input
+                  type='file'
+                  ref={fileInputRef}
+                  onChange={handleFileChange}
+                  style={{ display: 'none' }}
+                />
+              </AssetUploader>
+              <AssetList>
+                {assets.map((asset, index) => (
+                  <AssetItem key={index}>
+                    <ImagePreview src={asset.url} alt={asset.name} />
+                    <span>{asset.name}</span>
+                  </AssetItem>
+                ))}
+              </AssetList>
+            </div>
+          )}
         </>
       )}
     </PaletteContainer>
