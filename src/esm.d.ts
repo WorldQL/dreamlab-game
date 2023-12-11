4c85c0dc97d2ae6c40853cf6e6974350964e5c9b
Jack Baron <jackmbaron@gmail.com>
2023-12-11 15:48:09 +0000

    fix lints

--- src/esm.d.ts
+++ src/esm.d.ts
@@ -10,10 +10,6 @@ declare module 'https://esm.sh/v136/react@18.2.0/jsx-runtime' {
   export * from 'react/jsx-runtime'
 }
 
-declare module 'https://esm.sh/v136/react-dom@18.2.0/client' {
-  export * from 'react-dom/client'
-}
-
 declare module 'https://esm.sh/v136/styled-components@6.1.1' {
   export { default } from 'styled-components'
   export * from 'styled-components'
