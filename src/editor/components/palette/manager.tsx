973a3a383c39a96bec8acb2273172f45ecde0130
Christopher G <devcodedred@gmail.com>
2023-12-20 12:07:12 -0600

    svg and spacing changes

--- src/editor/components/palette/manager.tsx
+++ src/editor/components/palette/manager.tsx
@@ -57,11 +57,35 @@ export const PaletteManager: React.FC<PaletteManagerProps> = ({
         onClick={() => setIsCollapsed(prev => !prev)}
         style={{ left: '0.3rem' }}
       >
-        {isCollapsed ? '✚' : '─'}
+        {isCollapsed ? (
+          <svg
+            height='16'
+            viewBox='0 0 256 512'
+            width='8'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path d='M9.4 278.6c-12.5-12.5-12.5-32.8 0-45.3l128-128c9.2-9.2 22.9-11.9 34.9-6.9s19.8 16.6 19.8 29.6l0 256c0 12.9-7.8 24.6-19.8 29.6s-25.7 2.2-34.9-6.9l-128-128z' />
+          </svg>
+        ) : (
+          <svg
+            height='16'
+            viewBox='0 0 256 512'
+            width='8'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path d='M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z' />
+          </svg>
+        )}
       </CollapseButton>
       {!isCollapsed && (
         <>
-          <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-around',
+              marginBottom: '12px',
+            }}
+          >
             {Object.values(CATEGORIES).map(category => (
               <Button
                 key={category}
