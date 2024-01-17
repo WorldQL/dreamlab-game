9ac4ae4ea10daefd5e755c3ba0b0958bc577abf7
Christopher G <devcodedred@gmail.com>
2024-01-17 14:31:22 -0600

    better cmd+z window listening

--- src/editor/components/ui.tsx
+++ src/editor/components/ui.tsx
@@ -21,15 +21,16 @@ export const renderUI = (
   const ui = render(
     game,
     <StyleSheetManager target={styles}>
-      <History history={history} selector={selector} />
-      {editDetails && <CLICommand details={editDetails} />}
+      <History history={history} selector={selector}>
+        {editDetails && <CLICommand details={editDetails} />}
 
-      <SceneList history={history} selector={selector} />
-      <PaletteManager
-        history={history}
-        navigator={navigator}
-        selector={selector}
-      />
+        <SceneList history={history} selector={selector} />
+        <PaletteManager
+          history={history}
+          navigator={navigator}
+          selector={selector}
+        />
+      </History>
     </StyleSheetManager>,
     { interactable: false },
   )
