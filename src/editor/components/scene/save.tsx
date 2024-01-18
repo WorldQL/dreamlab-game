e97d41cfaa30e4f645be4b37b3538c36c78d0984
Christopher G <devcodedred@gmail.com>
2024-01-18 11:38:45 -0600

    remove unused import

--- src/editor/components/scene/save.tsx
+++ src/editor/components/scene/save.tsx
@@ -1,7 +1,7 @@
 import type { SpawnableEntity } from '@dreamlab.gg/core'
 import axios from 'axios'
 import type { FC, ReactNode } from 'https://esm.sh/v136/react@18.2.0'
-import React, { useCallback, useState } from 'https://esm.sh/v136/react@18.2.0'
+import { useCallback, useState } from 'https://esm.sh/v136/react@18.2.0'
 import { styled } from 'https://esm.sh/v136/styled-components@6.1.6'
 import type { EditDetails } from '../../editor'
 import { Button } from '../ui/buttons'
