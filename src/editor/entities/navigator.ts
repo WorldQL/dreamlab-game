5bd7a2b52b9c374c25e8a167cc0af5c212afa944
Jack Baron <jackmbaron@gmail.com>
2024-02-14 12:53:58 +0000

    cleanup navigator

--- src/editor/entities/navigator.ts
+++ src/editor/entities/navigator.ts
@@ -1,83 +1,64 @@
 import type { RenderTime } from '@dreamlab.gg/core'
-import { Entity, Game } from '@dreamlab.gg/core'
-// import { createEntity } from '@dreamlab.gg/core'
-import type { Camera } from '@dreamlab.gg/core/dist/entities'
-import type { InputManager } from '@dreamlab.gg/core/dist/input'
-import type { LooseVector } from '@dreamlab.gg/core/dist/math'
+import { Entity } from '@dreamlab.gg/core'
 import { Vec } from '@dreamlab.gg/core/dist/math'
-import { deferUntilPlayer } from '@dreamlab.gg/core/dist/utils'
-import type { Debug, Ref } from '@dreamlab.gg/core/dist/utils'
-import { game } from '@dreamlab.gg/core/labs'
+import type { Ref } from '@dreamlab.gg/core/dist/utils'
+import { camera, canvas, container, game, inputs } from '@dreamlab.gg/core/labs'
 import type { Vector } from 'matter-js'
 import { LOCKED_TAG } from '../editor'
 import type { Selector } from './select'
 
 export class Navigator extends Entity {
   #position: Matter.Vector = Vec.create()
-  public isDragging = false
-  public pressedEntity = false
-  public previousCursorPosition: Vector | null | undefined = null
-  public isSpacePressed = false
+  #isDragging = false
+  #pressedEntity = false
+  #previousCursorPosition: Vector | null | undefined = null
+  #isSpacePressed = false
 
   public constructor(
     public editorEnabled: Ref<boolean>,
     public selector: Selector,
   ) {
     super()
-    const _game = game('client')
-    if (_game) {
-      _game.client.inputs.addListener('Space', this.onSpace)
-      const canvas = _game.client.render.canvas
-      canvas.addEventListener('wheel', this.onWheel)
-      canvas.addEventListener('mousemove', this.onMouseMove)
-      canvas.addEventListener('mousedown', this.onMouseDown)
-      canvas.addEventListener('mouseup', this.onMouseUp)
-      canvas.addEventListener('mouseleave', this.onMouseLeave)
-    }
+
+    inputs().addListener('Space', this.#onSpace)
+
+    const $canvas = canvas()
+    $canvas.addEventListener('wheel', this.#onWheel)
+    $canvas.addEventListener('mousemove', this.#onMouseMove)
+    $canvas.addEventListener('mousedown', this.#onMouseDown)
+    $canvas.addEventListener('mouseup', this.#onMouseUp)
+    $canvas.addEventListener('mouseleave', this.#onMouseLeave)
   }
 
-  private readonly onMouseDown = (ev: MouseEvent) => {
+  readonly #onMouseDown = (ev: MouseEvent) => {
     if (!this.editorEnabled.value) return
-    if (ev.button === 2 || ev.button === 1 || (ev.button === 0 && this.isSpacePressed)) {
+    if (ev.button === 2 || ev.button === 1 || (ev.button === 0 && this.#isSpacePressed)) {
       this.selector.deselect()
-      this.isDragging = true
-      this.previousCursorPosition = game()?.client?.inputs.getCursor('screen')
+      this.#isDragging = true
+      this.#previousCursorPosition = game()?.client?.inputs.getCursor('screen')
     }
   }
 
-  private readonly onMouseMove = () => {
-    const _game = game('client')
-    if (_game) {
-      if (!this.isDragging || !this.previousCursorPosition) return
+  readonly #onMouseMove = () => {
+    if (!this.#isDragging || !this.#previousCursorPosition) return
 
-      const mousePosition = game()?.client?.inputs.getCursor('screen')
-      if (!mousePosition) return
+    const mousePosition = game()?.client?.inputs.getCursor('screen')
+    if (!mousePosition) return
 
-      const cursorDelta = Vec.sub(this.previousCursorPosition, mousePosition)
-      const amplifiedMovement = Vec.div(cursorDelta, _game.client.render.camera.scale)
-      const newPosition = Vec.add(this.position(), amplifiedMovement)
+    const cursorDelta = Vec.sub(this.#previousCursorPosition, mousePosition)
+    const amplifiedMovement = Vec.div(cursorDelta, camera().scale)
+    const newPosition = Vec.add(this.position(), amplifiedMovement)
+    camera().target = { position: newPosition }
 
-      _game.client.render.camera.target = {
-        position: newPosition,
-      }
-
-      this.previousCursorPosition = mousePosition
-      this.setPosition(newPosition)
-    }
+    this.#previousCursorPosition = mousePosition
+    this.setPosition(newPosition)
   }
 
-  private readonly onWheel = (ev: WheelEvent) => {
+  readonly #onWheel = (ev: WheelEvent) => {
     if (!this.editorEnabled.value || ev.ctrlKey) return
-    const _game = game('client')
-    if (!_game) {
-      return
-    }
-
     ev.preventDefault()
-    const amplifiedMovement = Vec.div(
-      Vec.create(ev.deltaX, ev.deltaY),
-      _game.client.render.camera.scale,
-    )
+
+    const amplifiedMovement = Vec.div(Vec.create(ev.deltaX, ev.deltaY), camera().scale)
     if (ev.shiftKey) {
       // swap the X and Y values so that it scrolls like Figma
       const tempX = amplifiedMovement.x
@@ -86,24 +67,23 @@ export class Navigator extends Entity {
     }
 
     const newPosition = Vec.add(this.position(), amplifiedMovement)
-
-    _game.client.render.camera.target = { position: newPosition }
+    camera().target = { position: newPosition }
     this.setPosition(newPosition)
   }
 
-  private readonly onMouseUp = () => {
-    this.isDragging = false
-    this.pressedEntity = false
-    this.previousCursorPosition = null
+  readonly #onMouseUp = () => {
+    this.#isDragging = false
+    this.#pressedEntity = false
+    this.#previousCursorPosition = null
   }
 
-  private readonly onMouseLeave = () => {
-    this.isDragging = false
-    this.previousCursorPosition = null
+  readonly #onMouseLeave = () => {
+    this.#isDragging = false
+    this.#previousCursorPosition = null
   }
 
-  private readonly onSpace = (pressed: boolean) => {
-    this.isSpacePressed = pressed
+  readonly #onSpace = (pressed: boolean) => {
+    this.#isSpacePressed = pressed
   }
 
   public setPosition(newPosition: Vector) {
@@ -114,46 +94,38 @@ export class Navigator extends Entity {
     return Vec.clone(this.#position)
   }
 
-  public override onRenderFrame({ delta }: RenderTime): void {
-    const _game = game('client')
-    if (!_game) {
-      return
-    }
-
+  public override onRenderFrame(_: RenderTime): void {
+    const $game = game('client', true)
     if (this.editorEnabled.value) {
-      const cursorPosition = _game.client.inputs.getCursor()
+      const cursorPosition = inputs().getCursor()
       let cursorStyle = 'default'
 
       if (cursorPosition) {
-        const query = _game.queryPosition(cursorPosition)
+        const query = $game.queryPosition(cursorPosition)
         const queryResults = query.map(({ definition: { tags } }) => tags)
         const isCursorOverNonLockedEntity = queryResults.some(tags => !tags?.includes(LOCKED_TAG))
 
-        if (this.isDragging && !this.pressedEntity) {
+        if (this.#isDragging && !this.#pressedEntity) {
           cursorStyle = 'grabbing'
-        } else if (this.isSpacePressed) {
+        } else if (this.#isSpacePressed) {
           cursorStyle = 'grab'
-        } else if (!this.isDragging && isCursorOverNonLockedEntity) {
+        } else if (!this.#isDragging && isCursorOverNonLockedEntity) {
           cursorStyle = 'pointer'
         }
       }
 
-      _game.client.render.container.style.cursor = cursorStyle
-    } else _game.client.render.container.style.cursor = 'default'
+      container().style.cursor = cursorStyle
+    } else container().style.cursor = 'default'
   }
 
   public teardown(): void {
-    const _game = game('client')
-    if (!_game) {
-      return
-    }
+    inputs().removeListener('Space', this.#onSpace)
 
-    const canvas = _game.client.render.canvas
-    canvas.removeEventListener('mousedown', this.onMouseDown)
-    canvas.removeEventListener('mouseup', this.onMouseUp)
-    canvas.removeEventListener('mousemove', this.onMouseMove)
-    canvas.removeEventListener('mouseleave', this.onMouseLeave)
-    canvas.removeEventListener('wheel', this.onWheel)
-    _game.client.inputs.removeListener('Space', this.onSpace)
+    const $canvas = canvas()
+    $canvas.removeEventListener('mousedown', this.#onMouseDown)
+    $canvas.removeEventListener('mouseup', this.#onMouseUp)
+    $canvas.removeEventListener('mousemove', this.#onMouseMove)
+    $canvas.removeEventListener('mouseleave', this.#onMouseLeave)
+    $canvas.removeEventListener('wheel', this.#onWheel)
   }
 }
