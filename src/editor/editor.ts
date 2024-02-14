fa45eabc075dac6bedbcddcda5483fc7f4aecbe3
Jack Baron <jackmbaron@gmail.com>
2024-02-14 13:25:45 +0000

    cleanup editor

--- src/editor/editor.ts
+++ src/editor/editor.ts
@@ -1,6 +1,5 @@
-// import { createEntity } from '@dreamlab.gg/core'
 import { Entity } from '@dreamlab.gg/core'
-import { game } from '@dreamlab.gg/core/labs'
+import { camera, game, inputs } from '@dreamlab.gg/core/labs'
 import { deferUntilPlayer, ref } from '@dreamlab.gg/core/utils'
 import type { ToServerPacket } from '../packets'
 import type { Action } from './components/history'
@@ -25,64 +24,65 @@ export interface EditDetails {
 }
 
 export class Editor extends Entity {
+  #enabled = ref<boolean>(false)
+  #actionHistory = { value: [] as Action[] }
+  #history = {
+    record: (action: Action) => {
+      this.#actionHistory.value.push(action)
+    },
+    undo: () => this.#actionHistory.value.pop(),
+    getActions: () => this.#actionHistory.value,
+  }
+
+  #selector = new Selector(this.#enabled, this.#history, this.sendPacket)
+  #navigator = new Navigator(this.#enabled, this.#selector)
+
   public constructor(
     public sendPacket?: (packet: ToServerPacket) => void,
     public editDetails?: EditDetails,
   ) {
     super()
-    const _game = game('client')
-    if (!_game) return
+    const $game = game('client', true)
 
-    _game.instantiate(this.selector)
-    _game.instantiate(this.navigator)
+    $game.instantiate(this.#selector)
+    $game.instantiate(this.#navigator)
 
-    const inputs = _game.client?.inputs
-    inputs?.registerInput(EditorInputs.DeleteEntity, 'Delete Entity', 'Backspace')
+    inputs().registerInput(EditorInputs.DeleteEntity, 'Delete Entity', 'Backspace')
+    inputs().registerInput(EditorInputs.MoveForewards, 'Move Into Foreground', 'BracketRight')
+    inputs().registerInput(EditorInputs.MoveBackwards, 'Move Into Background', 'BracketLeft')
+    inputs().registerInput(EditorInputs.ToggleTiling, 'Toggle Sprite Tiling', 'Backslash')
 
-    inputs?.registerInput(EditorInputs.MoveForewards, 'Move Into Foreground', 'BracketRight')
-    inputs?.registerInput(EditorInputs.MoveBackwards, 'Move Into Background', 'BracketLeft')
-
-    inputs?.registerInput(EditorInputs.ToggleTiling, 'Toggle Sprite Tiling', 'Backslash')
-
-    deferUntilPlayer(_game, player => {
+    deferUntilPlayer(player => {
       player.events.addListener('onToggleNoclip', noclip => {
-        this.enabled.value = noclip
+        this.#enabled.value = noclip
         if (noclip) {
-          _game.client.render.camera.smoothing = 0.02
-          this.navigator.setPosition(player.position)
+          camera().smoothing = 0.02
+          this.#navigator.setPosition(player.position)
         } else {
-          player.teleport(this.navigator.position())
-          _game.client.render.camera.target = player
-          _game.client.render.camera.smoothing = 0.125
-          inputs?.enable('mouse', 'editor')
-          this.selector.deselect()
-          _game.client?.inputs.setKey('MouseLeft', false)
+          player.teleport(this.#navigator.position())
+          camera().target = player
+          camera().smoothing = 0.125
+
+          inputs().enable('mouse', 'editor')
+          this.#selector.deselect()
+          inputs().setKey('MouseLeft', false)
         }
       })
     })
 
-    const { container } = renderUI(_game, this.selector, this.navigator, this.history, editDetails)
+    const { container } = renderUI(this.#selector, this.#navigator, this.#history, editDetails)
     container.style.display = 'none'
 
-    deferUntilPlayer(_game, player => {
+    deferUntilPlayer(player => {
       player.events.addListener('onToggleNoclip', noclip => {
         container.style.display = noclip ? '' : 'none'
       })
     })
   }
-  public enabled = ref<boolean>(false)
-  public actionHistory = { value: [] as Action[] }
-  public history = {
-    record: (action: Action) => {
-      this.actionHistory.value.push(action)
-    },
-    undo: () => this.actionHistory.value.pop(),
-    getActions: () => this.actionHistory.value,
-  }
-  public selector = new Selector(this.enabled, this.history, this.sendPacket)
-  public navigator = new Navigator(this.enabled, this.selector)
 
   public teardown(): void {
-    game('client')?.destroy(this.selector)
+    const $game = game('client', true)
+    $game.destroy(this.#selector)
+    $game.destroy(this.#navigator)
   }
 }
