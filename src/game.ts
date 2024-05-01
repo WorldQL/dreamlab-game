dcb1d7d616e5dc73b0b11799fdbeb5e8c01bf28e
Jackson Roberts <jacksonroberts25@gmail.com>
2024-05-01 14:40:56 -0600

    if this work i stg.

--- src/game.ts
+++ src/game.ts
@@ -53,16 +53,17 @@ export const setup = async (container: HTMLDivElement) => {
       nickname: playerInfo?.nickname ?? 'Player',
     },
     graphicsOptions: {
-      backgroundAlpha: 1,
+      backgroundAlpha: 0,
       antialias: true,
-      resolution: 2.5,
+      resolution: 2,
+      powerPreference: 'high-performance',
     },
   })
 
   console.log("MULTISAMPLE: " + game.client.render.app.renderer.multisample)
 
   setTimeout(() => {
-    console.log('RESOLUTION IS 2.5, pixel ratio:', window.devicePixelRatio)
+    console.log('RESOLUTION IS 2 WITH HP, pixel ratio:', window.devicePixelRatio)
     console.log('calling resize')
     window.dispatchEvent(new Event('resize'))
     console.log('called resize. Render type:', game.client.render.app.renderer.type)
